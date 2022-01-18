b4c2406eb71d0e6fab152ec33260b2c93fe6b3d1
김영후 <81841082+wherehows@users.noreply.github.com>
2022-01-18 21:23:08 +0900

    Time: 284 ms (72.48%), Space: 69.7 MB (59.63%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.js
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.js
@@ -1,22 +1,84 @@
-var maxProbability = function(n, edges, succProb, start, end) {
-  const p = Array(n).fill(0);
-  const graph = p.reduce((m, _, i) => m.set(i, []), new Map());
-  edges.forEach(([u, v], i) => {
-    graph.get(u).push([v, succProb[i]]);
-    graph.get(v).push([u, succProb[i]]);
-  });
-  
-  const queue = [[start, 1]];
-  p[start] = 1;
-  
-  for (let [node, currP] of queue) {   
-    for (let [adj, nextP] of graph.get(node)) {
-      if (currP * nextP > p[adj]) {
-        p[adj] = currP * nextP;
-        queue.push([adj, p[adj]]);
-      }
+const maxProbability = (n, edges, succProb, start, end) => {
+    const probs = Array(n).fill(0)
+    const pq = PQ( probs )
+    const edgeMap = edges.reduce( ( map, [ a, b ], i ) => {
+        map[ a ].push( [ b, succProb[i] ] )
+        map[ b ].push( [ a, succProb[i] ] )
+        return map
+    }, Array.from({length:n},() => []) )
+
+    pq.update( start, 1 )
+
+    while ( ! pq.empty() ) {
+        const nodeID = pq.takeBest()
+        if ( nodeID === end ) return probs[end]
+        if ( ! edgeMap[ nodeID ] ) continue
+
+        edgeMap[ nodeID ].forEach( ([ toID, prob ]) => {
+            if ( probs[nodeID] * prob > probs[toID] ) {
+                pq.update( toID, probs[nodeID] * prob )
+            }
+        })
     }
-  }
-  
-  return p[end];
-};
+
+    return probs[end]
+}
+
+const PQ = ( probs ) => {
+    const heap = []
+    const vertexMap = probs.reduce( ( obj, prob, i ) => {
+        obj[ i ] = i
+        heap[ i ] = i
+        return obj
+    }, {} )
+    
+    const empty = () => heap.length === 0
+    const parent = i => ~~( i / 2 )
+    const leftChild = i => i * 2 + 1
+    const rightChild = i => i * 2 + 2
+    
+    const takeBest = () => {
+        swap( 0, heap.length - 1 )
+        const best = heap.pop()
+        
+        siftDown( 0 )
+        
+        return best
+    }
+    const update = ( vertex, value ) => {
+        probs[ vertex ] = value
+        
+        siftUp( vertexMap[ vertex ] )
+
+    }
+    
+    const siftDown = ( i ) => {
+        const l = leftChild( i )
+        const r = rightChild( i )
+        const c = ! r ? l : probs[heap[l]] > probs[heap[r]] ? l : r
+
+        if ( c && probs[heap[c]] > probs[heap[i]] ) {
+            swap( c, i )
+            siftDown( c )
+        }
+    }
+    const siftUp = ( i ) => {
+        if ( 0 === i ) return
+        const p = parent( i )
+        
+        if ( probs[heap[p]] < probs[heap[i]] ) {
+            swap( p, i )
+            siftUp( p )
+        }
+    }
+    const swap = ( i, j ) => {
+        const hold = heap[i]
+        heap[i] = heap[j]
+        heap[j] = hold
+        
+        vertexMap[heap[i]] = i
+        vertexMap[heap[j]] = j
+    }
+    
+    return { empty, takeBest, update }
+}
