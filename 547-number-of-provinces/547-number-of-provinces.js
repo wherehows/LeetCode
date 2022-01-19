4bb7a1f78f636ad48205b84acfd686c540bca28c
김영후 <81841082+wherehows@users.noreply.github.com>
2022-01-19 22:48:07 +0900

    Time: 256 ms (5.02%), Space: 43 MB (11.95%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.js
+++ 547-number-of-provinces/547-number-of-provinces.js
@@ -1,27 +1,33 @@
-var findCircleNum = function (M) {
-  let res = 0;
-  const dsu = new DSU(M.length);
-  for (let row = 0; row < M.length; row++) {
-    for (let col = 0; col < M[0].length; col++) {
-      if (M[row][col] === 1) {
-        dsu.union(row, col);
-      }
+var findCircleNum = function(M) {
+  
+    class UnionFind {
+        constructor(n) {
+            this.graph = [...Array(n)].map((_, i) => i);
+            this.groups = n;
+        }
+        
+        find(id) {
+            if(this.graph[id] === id) return id;
+            this.graph[id] = this.find(this.graph[id]);
+            return this.graph[id];
+        }
+        
+        union(x, y) {
+            const rootX = this.find(x);
+            const rootY = this.find(y);
+            if(rootX !== rootY) {
+                this.graph[rootY] = rootX;
+                this.groups--;
+            }
+        }
     }
-  }
-  return new Set(M.map((m, i) => dsu.find(i))).size;
+
+    const N = M.length, unionfind = new UnionFind(N);
+    
+    for(let r = 0; r < N; r++) {
+        for(let c = 0; c < N; c++) {
+            if(M[r][c]) unionfind.union(r, c);
+        }
+    }
+    return unionfind.groups;
 };
-
-class DSU {
-  constructor(N) {
-    this.parent = [...new Array(N).keys()];
-  }
-  find(x) {
-    if (this.parent[x] !== x) this.parent[x] = this.find(this.parent[x]);
-    return this.parent[x];
-  }
-  union(x, y) {
-    this.parent[this.find(x)] = this.find(y);
-
-    console.log("x", x, "y", y, "this.parent", this.parent);
-  }
-}
