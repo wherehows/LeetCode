3cbd01d5d11640c0da4ae1ae0ccf8057708b923c
김영후 <81841082+wherehows@users.noreply.github.com>
2022-01-29 23:52:21 +0900

    Time: 330 ms (17.65%), Space: 79.8 MB (5.88%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.js
+++ 886-possible-bipartition/886-possible-bipartition.js
@@ -1,80 +1,42 @@
-/**
- * @param {number} n
- * @param {number[][]} dislikes
- * @return {boolean}
- */
-
-var possibleBipartition = function (n, dislikes) {
+var possibleBipartition = function (N, dislikes) {
   const edgeMap = dislikes.reduce((map, [a, b]) => {
-    if (map.has(a)) {
-      const arr1 = map.get(a);
-      arr1.push(b);
-    } else {
-      map.set(a, [b]);
-    }
-
-    if (map.has(b)) {
-      const arr1 = map.get(b);
-      arr1.push(a);
-    } else {
-      map.set(b, [a]);
-    }
-
+    map.set(a, [...(map.get(a) || []), b]);
+    map.set(b, [...(map.get(b) || []), a]);
     return map;
   }, new Map());
 
-  const setA = new Set();
-  const setB = new Set();
-  let res = true;
+  const visited = Array.from({ length: N }, () => false);
 
-  for (const num of edgeMap.keys()) {
-    if (setA.has(num) || setB.has(num)) continue;
-    setA.add(num);
-    check(num, "a");
+  const colors = Array.from({ length: N }, () => 0);
+
+  for (let v = 1; v <= N; v++) {
+    if (!dfs(v)) return false;
   }
 
-  return res;
+  return true;
 
-  function check(num, set) {
-    if (!res) return;
+  function dfs(vertex) {
+    if (visited[vertex]) return true;
 
-    if (set === "a") {
-      const nextVisits = edgeMap.get(num);
+    const currColor = new Set([1, 2]);
 
-      for (const next of nextVisits) {
-        if (setA.has(next)) {
-          res = false;
-          return;
-        }
+    const nextChild = edgeMap.get(vertex);
 
-        if (!setB.has(next)) {
-          setB.add(next);
-          check(next, "b");
-        }
+    if (nextChild) {
+      for (const child of nextChild) {
+        if (colors[child] === 1) currColor.delete(1);
+        if (colors[child] === 2) currColor.delete(2);
+      }
+
+      if (currColor.size === 0) return false;
+
+      colors[vertex] = Math.min(...currColor);
+      visited[vertex] = true;
+
+      for (const child of nextChild) {
+        if (!dfs(child)) return false;
       }
     }
-
-    if (set === "b") {
-      const nextVisits = edgeMap.get(num);
-
-      for (const next of nextVisits) {
-        if (setB.has(next)) {
-          res = false;
-          return;
-        }
-        if (!setA.has(next)) {
-          setA.add(next);
-          check(next, "a");
-        }
-      }
-    }
+    return true;
   }
 };
-
-console.log(
-  possibleBipartition(3, [
-    [1, 2],
-    [1, 3],
-    [2, 4],
-  ])
-);
