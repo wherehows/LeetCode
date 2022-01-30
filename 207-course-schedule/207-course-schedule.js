4a4a03f3aac823760cca9b5af02b0cfc30c8d485
김영후 <81841082+wherehows@users.noreply.github.com>
2022-01-30 19:38:04 +0900

    Time: 125 ms (54.83%), Space: 49.9 MB (7.85%) - LeetHub

--- 207-course-schedule/207-course-schedule.js
+++ 207-course-schedule/207-course-schedule.js
@@ -1,37 +1,32 @@
-/**
- * @param {number} numCourses
- * @param {number[][]} prerequisites
- * @return {boolean}
- */
 var canFinish = function (numCourses, prerequisites) {
-  const queue = [];
-  let visitedCount = 0;
-
-  const inDegrees = Array.from({ length: numCourses }, () => 0);
-
   const edgeMap = prerequisites.reduce((map, [a, b]) => {
     map.set(b, [...(map.get(b) || []), a]);
-    inDegrees[a]++;
-
     return map;
   }, new Map());
 
-  inDegrees.forEach((inDegree, i) => {
-    if (inDegree === 0) queue.push(i);
-  });
+  const visited = new Set();
+  const visiting = new Set();
 
-  while (queue.length) {
-    const vertex = queue.shift();
-    if (edgeMap.has(vertex)) {
-      const nextVertexs = edgeMap.get(vertex);
-      for (const nextVertex of nextVertexs) {
-        inDegrees[nextVertex]--;
-        if (inDegrees[nextVertex] === 0) queue.push(nextVertex);
+  for (const [a, b] of edgeMap) {
+    if (isCyclic(a)) return false;
+  }
+
+  return true;
+
+  function isCyclic(currentVertex) {
+    const nextVertics = edgeMap.get(currentVertex);
+    visiting.add(currentVertex);
+
+    if (nextVertics) {
+      for (const nextVertex of nextVertics) {
+        if (visited.has(nextVertex)) continue;
+        if (visiting.has(nextVertex)) return true;
+        if (isCyclic(nextVertex)) return true;
       }
     }
 
-    visitedCount++;
+    visited.add(currentVertex);
+    visiting.delete(currentVertex);
+    return false;
   }
-
-  return numCourses === visitedCount;
 };
