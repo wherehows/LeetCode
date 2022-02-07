7cffd347a153d099732d7e366691b48a8598e97e
김영후 <81841082+wherehows@users.noreply.github.com>
2022-02-07 21:56:31 +0900

    Time: 196 ms (53.45%), Space: 60.1 MB (9.48%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.js
+++ 886-possible-bipartition/886-possible-bipartition.js
@@ -1,42 +1,53 @@
-var possibleBipartition = function (N, dislikes) {
+/**
+ * @param {number} n
+ * @param {number[][]} dislikes
+ * @return {boolean}
+ */
+var possibleBipartition = function (n, dislikes) {
   const edgeMap = dislikes.reduce((map, [a, b]) => {
     map.set(a, [...(map.get(a) || []), b]);
     map.set(b, [...(map.get(b) || []), a]);
     return map;
   }, new Map());
 
-  const visited = Array.from({ length: N }, () => false);
+  const visited = Array.from({ length: n + 1 }, () => false);
+  const colors = Array.from({ length: n + 1 }, () => 0);
 
-  const colors = Array.from({ length: N }, () => 0);
-
-  for (let v = 1; v <= N; v++) {
-    if (!dfs(v)) return false;
+  for (let i = 1; i <= n; i++) {
+    if (visited[i]) continue;
+    if (hasWrongColor(i)) return false;
   }
 
   return true;
 
-  function dfs(vertex) {
-    if (visited[vertex]) return true;
+  function hasWrongColor(vertex) {
+    const remainColors = new Set([1, 2]);
+    const nextVertices = edgeMap.get(vertex);
 
-    const currColor = new Set([1, 2]);
-
-    const nextChild = edgeMap.get(vertex);
-
-    if (nextChild) {
-      for (const child of nextChild) {
-        if (colors[child] === 1) currColor.delete(1);
-        if (colors[child] === 2) currColor.delete(2);
+    if (nextVertices) {
+      for (const nextVertex of nextVertices) {
+        if (colors[nextVertex] === 1) remainColors.delete(1);
+        if (colors[nextVertex] === 2) remainColors.delete(2);
       }
 
-      if (currColor.size === 0) return false;
+      if (remainColors.size === 0) return true;
 
-      colors[vertex] = Math.min(...currColor);
       visited[vertex] = true;
+      colors[vertex] = [...remainColors][0];
 
-      for (const child of nextChild) {
-        if (!dfs(child)) return false;
+      for (const nextVertex of nextVertices) {
+        if (visited[nextVertex] === true) continue;
+        if (hasWrongColor(nextVertex)) return true;
       }
     }
-    return true;
+    return false;
   }
 };
+
+console.log(
+  possibleBipartition(3, [
+    [1, 2],
+    [1, 3],
+    [2, 3],
+  ])
+);
