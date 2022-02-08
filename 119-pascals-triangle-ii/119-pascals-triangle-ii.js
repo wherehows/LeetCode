125174257205dd35fc614cf3a0a3f242ed8fa781
김영후 <81841082+wherehows@users.noreply.github.com>
2022-02-08 14:45:12 +0900

    Time: 82 ms (49.89%), Space: 41.7 MB (21.46%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.js
@@ -1,21 +1,26 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
 var getRow = function (rowIndex) {
-  if (rowIndex === 0) return [1];
-  if (rowIndex === 1) return [1, 1];
+  const zero = [1];
+  const first = [1, 1];
 
-  let prior = [1, 1];
-  let next = [];
+  if (rowIndex <= 1) return rowIndex === 0 ? zero : first;
 
-  for (let i = 2; i < rowIndex + 1; i++) {
-    next = [];
+  return helper(1, [1, 1]);
 
-    for (let j = 0; j < i + 1; j++) {
-      if (j === 0) next.push(1);
-      else if (j === i) next.push(1);
-      else next.push(prior[j - 1] + prior[j]);
+  function helper(currentLevel, prior) {
+    if (currentLevel === rowIndex) return prior;
+
+    const next = [1];
+
+    for (let index = 0; index < currentLevel; index++) {
+      next.push(prior[index] + prior[index + 1]);
     }
 
-    prior = next;
-  }
+    next.push(1);
 
-  return next;
+    return helper(currentLevel + 1, next);
+  }
 };
