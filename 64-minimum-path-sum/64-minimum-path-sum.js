0ecc4468ce91d098d0092dbb6c9d5fec662156d0
김영후 <81841082+wherehows@users.noreply.github.com>
2022-02-10 14:36:30 +0900

    Time: 111 ms (46.01%), Space: 44.7 MB (22.95%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.js
+++ 64-minimum-path-sum/64-minimum-path-sum.js
@@ -1,34 +1,32 @@
 var minPathSum = function(grid) {
-	// Get the two dimensions of the grid
-    const n = grid.length;
-    const m = grid[0].length;
+    const height = grid.length;
+    const width = grid[0].length;
     
-	// Calculate the distance travelled within the first column
-	// This is because each square depends on the one above
-	// And the one to the left. However there is nothing left
-	// of the first column so we can calculate it by adding
-	// the current square to the square above it
-    for(let i=1; i<n; i++) {
-        grid[i][0] += grid[i-1][0];
-    }
+    const map = (new Array(grid.length).fill()).map(e => new Array(grid[0].length).fill());
     
-	// The same goes for the first row. There is nothing above the 
-	// first row. So we just calculate the distance by what is to the left
-	// of it
-    for(let j=1; j<m; j++) {
-        grid[0][j] += grid[0][j-1];
-    }
-    
-	// Start one row and one column in because we've precomputed
-	// those above
-    for(let i=1; i<n; i++) {
-        for(let j=1; j<m; j++) {
-			// The distance to the grid at i,j is equal to itself plus the minimum
-			// of the two grid spaces (one above, one to the left)
-            grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+    const minPathFromPoint = (row, column) => {
+        if(row>= height || column >= width){
+            return Number.MAX_SAFE_INTEGER;
         }
+        
+        if(map[row][column]) {
+            return map[row][column];
+        }
+        
+        if(row === height-1 && column === width-1){
+            return grid[row][column];
+        }
+        
+        let current = 0;
+        const toRight = minPathFromPoint(row, column+1);
+        const toBottom = minPathFromPoint(row+1, column);
+    
+        let result = Math.min(toRight, toBottom)+grid[row][column];
+        
+        map[row][column] = result;
+        
+        return result;
     }
     
-	// Return the distance bottom right corner
-    return grid[n-1][m-1];
+    return minPathFromPoint(0,0);
 };
