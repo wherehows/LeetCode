cb99ba1be1024b451dfe8f3cdb9acd4798a832f9
김영후 <81841082+wherehows@users.noreply.github.com>
2022-02-10 15:41:29 +0900

    Time: 130 ms (27.16%), Space: 45.3 MB (18.85%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.js
+++ 64-minimum-path-sum/64-minimum-path-sum.js
@@ -1,32 +1,31 @@
-var minPathSum = function(grid) {
-    const height = grid.length;
-    const width = grid[0].length;
-    
-    const map = (new Array(grid.length).fill()).map(e => new Array(grid[0].length).fill());
-    
-    const minPathFromPoint = (row, column) => {
-        if(row>= height || column >= width){
-            return Number.MAX_SAFE_INTEGER;
-        }
-        
-        if(map[row][column]) {
-            return map[row][column];
-        }
-        
-        if(row === height-1 && column === width-1){
-            return grid[row][column];
-        }
-        
-        let current = 0;
-        const toRight = minPathFromPoint(row, column+1);
-        const toBottom = minPathFromPoint(row+1, column);
-    
-        let result = Math.min(toRight, toBottom)+grid[row][column];
-        
-        map[row][column] = result;
-        
-        return result;
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function (grid) {
+  const rowLength = grid[0].length;
+  const colLength = grid.length;
+
+  const map = Array.from({ length: colLength }, () =>
+    Array.from({ length: rowLength }, () => 0)
+  );
+
+  map[0][0] = grid[0][0];
+
+  for (let n = 1; n < rowLength; n++) {
+    map[0][n] = grid[0][n] + map[0][n - 1];
+  }
+
+  for (let m = 1; m < colLength; m++) {
+    map[m][0] = grid[m][0] + map[m - 1][0];
+  }
+
+  for (let m = 1; m < colLength; m++) {
+    for (let n = 1; n < rowLength; n++) {
+      if (map[m - 1][n] > map[m][n - 1]) map[m][n] = map[m][n - 1] + grid[m][n];
+      else map[m][n] = map[m - 1][n] + grid[m][n];
     }
-    
-    return minPathFromPoint(0,0);
+  }
+
+  return map[colLength - 1][rowLength - 1];
 };
