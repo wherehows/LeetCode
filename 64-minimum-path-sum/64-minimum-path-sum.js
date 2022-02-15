58b06f0d5a7f0b9d0b1488951bfbdc96b879cd4d
김영후 <81841082+wherehows@users.noreply.github.com>
2022-02-15 17:13:56 +0900

    Time: 85 ms (69.06%), Space: 47.7 MB (11.62%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.js
+++ 64-minimum-path-sum/64-minimum-path-sum.js
@@ -3,29 +3,28 @@
  * @return {number}
  */
 var minPathSum = function (grid) {
-  const rowLength = grid[0].length;
-  const colLength = grid.length;
+  const m = grid[0].length;
+  const n = grid.length;
 
-  const map = Array.from({ length: colLength }, () =>
-    Array.from({ length: rowLength }, () => 0)
+  const minGrid = Array.from({ length: n }, () =>
+    Array.from({ length: m }, () => undefined)
   );
 
-  map[0][0] = grid[0][0];
+  return helper([0, 0]);
 
-  for (let n = 1; n < rowLength; n++) {
-    map[0][n] = grid[0][n] + map[0][n - 1];
+  function helper(pos) {
+    const [x, y] = pos;
+    if (x >= m || y >= n) return Number.MAX_SAFE_INTEGER;
+    if (x === m - 1 && y === n - 1) return grid[y][x];
+    if (minGrid[y][x]) return minGrid[y][x];
+
+    const currentPosValue = grid[y][x];
+
+    const minValueOfRight = helper([x + 1, y]);
+    const minValueOfDown = helper([x, y + 1]);
+
+    minGrid[y][x] = Math.min(minValueOfRight, minValueOfDown) + currentPosValue;
+
+    return minGrid[y][x];
   }
-
-  for (let m = 1; m < colLength; m++) {
-    map[m][0] = grid[m][0] + map[m - 1][0];
-  }
-
-  for (let m = 1; m < colLength; m++) {
-    for (let n = 1; n < rowLength; n++) {
-      if (map[m - 1][n] > map[m][n - 1]) map[m][n] = map[m][n - 1] + grid[m][n];
-      else map[m][n] = map[m - 1][n] + grid[m][n];
-    }
-  }
-
-  return map[colLength - 1][rowLength - 1];
 };
