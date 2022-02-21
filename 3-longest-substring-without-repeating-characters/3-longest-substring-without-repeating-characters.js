921a0ef0577c066a7e1643e14e1ef998e2484436
김영후 <81841082+wherehows@users.noreply.github.com>
2022-02-21 20:00:44 +0900

    Time: 131 ms (65.92%), Space: 49 MB (18.09%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -3,23 +3,12 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  let res = 0;
   let left = 0;
+  let memo = {};
 
-  let subString = s[left];
-
-  for (let right = 0; right < s.length; right++) {
-    if (subString.includes(s[right])) {
-      const indexOfCurrentChar = subString.indexOf(s[right]);
-
-      subString = subString.slice(indexOfCurrentChar + 1) + s[right];
-      left = indexOfCurrentChar + 1;
-    } else {
-      subString += s[right];
-    }
-
-    res = Math.max(res, subString.length);
-  }
-
-  return res;
+  return s.split("").reduce((res, alphabet, index) => {
+    left = memo[alphabet] >= left ? memo[alphabet] + 1 : left;
+    memo[alphabet] = index;
+    return Math.max(res, index - left + 1);
+  }, 0);
 };
