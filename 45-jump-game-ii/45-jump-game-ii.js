6445df5f88a156e4584982009ad1753667e5434e
edward <81841082+wherehows@users.noreply.github.com>
2022-04-01 20:59:47 +0900

    Time: 90 ms (69.53%), Space: 44.3 MB (54.83%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.js
+++ 45-jump-game-ii/45-jump-game-ii.js
@@ -1,30 +1,16 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var jump = function (nums) {
-  let res = null;
-  let visited = new Set();
+  let jump = 0;
+  let oldMax = 0;
+  let newMax = 0;
 
-  helper(nums.length - 1, 0);
+  for (let i = 0; i < nums.length - 1; i++) {
+    newMax = Math.max(i + nums[i], newMax);
 
-  return res;
-
-  function helper(currentIndex, acc) {
-    if (currentIndex === 0) {
-      res = acc;
-      return;
-    }
-
-    for (let i = 0; i < currentIndex; i++) {
-      if (visited.has(i)) continue;
-
-      if (nums[i] >= currentIndex - i) {
-        helper(i, acc + 1);
-        visited.add(i);
-      }
-
-      if (res !== null) return;
+    if (oldMax === i) {
+      jump++;
+      oldMax = newMax;
     }
   }
+
+  return jump;
 };
