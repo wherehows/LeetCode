a312714f7f3026b5383de96d0b525e73c7312a0c
edward <81841082+wherehows@users.noreply.github.com>
2022-04-02 21:18:36 +0900

    Time: 205 ms (38.63%), Space: 49.5 MB (82.95%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.js
+++ 743-network-delay-time/743-network-delay-time.js
@@ -1,29 +1,27 @@
 var networkDelayTime = function (times, n, k) {
-  const timeCheck = Array.from({ length: n + 1 }, () => Infinity);
-  const visited = new Set();
+  const current = new Set();
+  const next = new Set();
+  const timeOfEachNode = Array.from(
+    { length: n + 1 },
+    () => Number.MAX_SAFE_INTEGER
+  );
+  timeOfEachNode[0] = 0;
+  timeOfEachNode[k] = 0;
+  current.add(k);
 
-  const edgeMap = times.reduce((map, [start, end, time]) => {
-    map.set(start, [...(map.get(start) || []), [end, time]]);
-    return map;
-  }, new Map());
-
-  visited.add(k);
-  helper(k, 0);
-
-  return visited.size !== n
-    ? -1
-    : Math.max(...timeCheck.filter((value) => value !== Infinity));
-
-  function helper(node, acc) {
-    const nextNodes = edgeMap.get(node);
-
-    if (nextNodes) {
-      for (const [nextNode, time] of nextNodes) {
-        if (time + acc >= timeCheck[nextNode] || nextNode === k) continue;
-        visited.add(nextNode);
-        timeCheck[nextNode] = acc + time;
-        helper(nextNode, time + acc);
-      }
+  for (let i = 0; i < n; i++) {
+    for (const [u, v, w] of times) {
+      if (!current.has(u)) continue;
+      if (timeOfEachNode[u] + w >= timeOfEachNode[v]) continue;
+      timeOfEachNode[v] = timeOfEachNode[u] + w;
+      next.add(v);
     }
+    current.clear();
+    [...next].forEach((value) => current.add(value));
+    next.clear();
   }
+
+  return Math.max(...timeOfEachNode) !== Number.MAX_SAFE_INTEGER
+    ? Math.max(...timeOfEachNode)
+    : -1;
 };
